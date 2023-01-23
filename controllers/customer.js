130401fd5b6e1402840ec2e7d2691795f367fbc3
son1122 <wongsatorn.n@hotmail.com>
2023-01-23 10:53:03 +0700

    edit func

--- controllers/customer.js
+++ controllers/customer.js
@@ -480,6 +480,21 @@ const dashboardData=(req,res)=>{
 
 }
 const claim=(req,res)=>{
+    user={}
+    const bearerHeader = req.headers["authorization"];
+    if (typeof bearerHeader !== "undefined") {
+        const bearer = bearerHeader.split(" ");
+        const bearerToken = bearer[1];
+        req.token = bearerToken;
+    }
+
+    jwt.verify(req.token, process.env.JWT_SECRET, (err, decodedUser) => {
+        if (err || !decodedUser)
+            return res.status(401).json({error: "Unauthorized Request"});
+
+        user = decodedUser;
+        console.log(user)
+    });
     CarClaim.findAll({where:{customerId:user.id}}).then(claim=>{
         res.json(claim)
     })
